9d88964e87dd2260e795d1a171c2d3cd10584ace
linx3273 <ayush96.gupta@gmail.com>
2022-04-15 23:30:35 +0530

    added method for rotating images

--- src/imageEdit.py
+++ src/imageEdit.py
@@ -32,26 +32,27 @@ class imageEdit:
 
 
     def edgeDetection(self):
-        self.grayscale = self.grayscale()
+        gray = self.grayscale()
     
-        vertical_sobel_filter=[[-1,-2,-1],[0,0,0],[1,2,1]]
-        hortizontal_sobel_filter=[[-1,0,1],[-2,0,2],[-1,0,1]]
+        vertical_sobel_filter = [[-1,-2,-1],[0,0,0],[1,2,1]]
+        hortizontal_sobel_filter = [[-1,0,1],[-2,0,2],[-1,0,1]]
         n,m,d = self.inp.shape
         
-        edges_img=np.zeros_like(self.grayscale)
+        edges_img=np.zeros_like(gray)
         for row in range(3, n-2):
             for col in range(3,m-2):
-                local_pixels=self.grayscale[row-1:row+2, col-1:col+2,0]
+                local_pixels = gray[row-1:row+2, col-1:col+2,0]
 
                 vertica_transformed_pixels=vertical_sobel_filter*local_pixels
-                vertical_score=vertica_transformed_pixels.sum()/4
+                vertical_score = vertica_transformed_pixels.sum()/4
 
-                horizonta_transformed_pxels=hortizontal_sobel_filter*local_pixels
-                hortizontal_score=horizonta_transformed_pxels.sum()/4
+                horizonta_transformed_pxels = hortizontal_sobel_filter*local_pixels
+                hortizontal_score = horizonta_transformed_pxels.sum()/4
 
-                edge_score=(vertical_score**2+hortizontal_score**2)**.5
-                edges_img[row, col]=[edge_score]*3
-        edges_img=edges_img/edges_img.max()
+                edge_score = (vertical_score**2+hortizontal_score**2)**.5
+                edges_img[row, col] = [edge_score]*3
+                
+        edges_img = edges_img/edges_img.max()
 
         self.outp = edges_img
 
@@ -88,7 +89,56 @@ class imageEdit:
 
 
     def rotate(self):
-        pass
+        ang = int(input("Enter angle in degrees: "))
+        SIN = np.sin(ang*np.pi/180)  # obtaining sin value
+        COS = np.cos(ang*np.pi/180)  # obtaining cos value
+
+        # defining height and width of the image
+        height,width= self.inp.shape[0],self.inp.shape[1]
+
+        # defining the height and width of the image post rotation
+        new_height = round(abs(self.inp.shape[0]*COS)+abs(self.inp.shape[1]*SIN))+1
+        new_width = round(abs(self.inp.shape[1]*COS)+abs(self.inp.shape[0]*SIN))+1
+
+        # creating template in output with new dimensions
+        self.outp = np.zeros((new_height,new_width,self.inp.shape[2]))
+
+        # finding centre about which image must be rotated
+        # calculated wrt original image
+        og_centre_height = round(((self.inp.shape[0]+1)/2)-1)
+        og_centre_width = round(((self.inp.shape[1]+1)/2)-1)
+
+        
+        # finding centre of the new image that will be obtained
+        # calcualted wrt new dimensions
+        new_centre_height = round(((new_height+1)/2)-1)
+        new_centre_width = round(((new_width+1)/2)-1)
+
+
+        for i in range(height):
+            for j in range(width):
+                # coords of pixel wrt the centre of original shape
+                y = self.inp.shape[0]-1-i-og_centre_height
+                x = self.inp.shape[1]-1-j-og_centre_width
+
+                # coords of pixel wrt to the rotated matrix
+                new_y = round(-x*SIN+y*COS)
+                new_x = round(x*COS+y*SIN)
+
+                # since image will be rotated the centre will change too
+                # to adjust that we need to chage new_x and new_y to the new centre
+                new_y = new_centre_height - new_y
+                new_x = new_centre_width - new_x
+
+                # adding if check to prevent any errors while procesing
+                if 0<=new_x < new_width and 0<=new_y<new_height and new_x>=0 and new_y>=0:
+                    self.outp[new_y,new_x,:] = self.inp[i,j,:]      # copying pixel value to given index             
+
+        # not sure why this line is required but without this I was getting value errors
+        # stating that the RGB values are floating point/ int32 etc and that they 
+        # are supposed to be of type uint8
+        self.outp = self.outp.astype(np.uint8)
+
 
 
 
