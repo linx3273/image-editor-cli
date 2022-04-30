60dcb0368f03efd0645c003789f3c34503208757
linx3273 <ayush96.gupta@gmail.com>
2022-05-01 00:08:20 +0530

    made some variables private in the class

--- src/imageEdit.py
+++ src/imageEdit.py
@@ -6,23 +6,23 @@ import re
 
 class imageEdit:
     def __init__(self,src,dest):
-        self.src = Path(src).resolve()
-        self.dest = Path(dest).resolve()
-        self.inp = None
-        self.outp = None
+        self.__src = Path(src).resolve()
+        self.__dest = Path(dest).resolve()
+        self.__inp = None
+        self.__outp = None
 
 
     def loadImg(self):
-        self.inp = mpimg.imread(self.src)
+        self.__inp = mpimg.imread(self.__src)
 
 
     def writeImg(self,format):
-        self.dest = Path(str(self.dest) + '/' + str(self.src.stem) + '_' + f'{format}' + str(self.src.suffix)).resolve()
-        mpimg.imsave(self.dest,self.outp)
+        self.__dest = Path(str(self.__dest) + '/' + str(self.__src.stem) + '_' + f'{format}' + str(self.__src.suffix)).resolve()
+        mpimg.imsave(self.__dest,self.__outp)
 
 
     def showOutput(self):
-        plt.imshow(self.outp)
+        plt.imshow(self.__outp)
         plt.show()
 
 
@@ -31,7 +31,7 @@ class imageEdit:
     
         vertical_sobel_filter = [[-1,-2,-1],[0,0,0],[1,2,1]]
         hortizontal_sobel_filter = [[-1,0,1],[-2,0,2],[-1,0,1]]
-        n,m,d = self.inp.shape
+        n,m,d = self.__inp.shape
         
         edges_img=np.zeros_like(gray)
         for row in range(3, n-2):
@@ -49,72 +49,72 @@ class imageEdit:
                 
         edges_img = edges_img/edges_img.max()
 
-        self.outp = edges_img
+        self.__outp = edges_img
 
-        return self.outp
+        return self.__outp
 
 
     def grayscale(self):
-        self.outp = np.zeros(self.inp.shape)
-        R = np.array(self.inp[:, :, 0])
-        G = np.array(self.inp[:, :, 1])
-        B = np.array(self.inp[:, :, 2])
+        self.__outp = np.zeros(self.__inp.shape)
+        R = np.array(self.__inp[:, :, 0])
+        G = np.array(self.__inp[:, :, 1])
+        B = np.array(self.__inp[:, :, 2])
 
         R = (R *.299)
         G = (G *.587)
         B = (B *.114)
 
         Avg = (R+G+B)
-        self.outp = self.inp.copy()
+        self.__outp = self.__inp.copy()
 
         for i in range(3):
-           self.outp[:,:,i] = Avg
+           self.__outp[:,:,i] = Avg
 
-        return self.outp
+        return self.__outp
 
 
     # def resize(self):
     #     w = float(input("Enter scaling factor for width: "))
     #     h = float(input("Enter scaling factor for height: "))
 
-    #     # self.outp = self.inp[::h,::w]
+    #     # self.__outp = self.__inp[::h,::w]
     
     #     if w>1 and h>1:
     #         # upscaling width and height
-    #         self.outp = self.inp.repeat(h,axis=0).repeat(w,axis=1)
+    #         self.__outp = self.__inp.repeat(h,axis=0).repeat(w,axis=1)
     #     elif w>1 and h<=1:
     #         # upscaling width, downscaling height
-    #         self.outp = self.inp[::1,::int(w)]
-    #         self.outp = self.outp.repeat(h,axis=0).repeat(1,axis=1)
+    #         self.__outp = self.__inp[::1,::int(w)]
+    #         self.__outp = self.__outp.repeat(h,axis=0).repeat(1,axis=1)
     #     elif w<=1 and h>1:
     #         # downscaling width, upscaling height
-    #         self.outp = self.inp[::int(h),::1]
-    #         self.outp = self.outp.repeat(w,axis=1).repeat(1,axis=0)
+    #         self.__outp = self.__inp[::int(h),::1]
+    #         self.__outp = self.__outp.repeat(w,axis=1).repeat(1,axis=0)
     #     else:
     #         # downscaling width and height
-    #         self.outp = self.inp[::int(1/h),::int(1/w)]
+    #         self.__outp = self.__inp[::int(1/h),::int(1/w)]
 
-    #     return self.outp
+    #     return self.__outp
 
             
     def upscale(self):
         self.factor = int(input("Enter scaling factor: "))
-        self.outp = self.inp.repeat(self.factor,axis=0).repeat(self.factor,axis=1)
+        self.__outp = self.__inp.repeat(self.factor,axis=0).repeat(self.factor,axis=1)
 
-        return self.outp
+        return self.__outp
 
 
     def downscale(self):
         self.factor = int(input("Enter downscale factor: "))
-        self.outp = self.inp[::self.factor,::self.factor]
+        self.__outp = self.__inp[::self.factor,::self.factor]
 
-        return self.outp
+        return self.__outp
         
 
     def flip(self):
-        self.outp = np.fliplr(self.inp)
+        self.__outp = np.fliplr(self.__inp)
 
-        self.outp
+        self.__outp
 
 
     def rotate(self):
@@ -123,19 +123,19 @@ class imageEdit:
         COS = np.cos(self.ang*np.pi/180)  # obtaining cos value
 
         # defining height and width of the image
-        height,width= self.inp.shape[0],self.inp.shape[1]
+        height,width= self.__inp.shape[0],self.__inp.shape[1]
 
         # defining the height and width of the image post rotation
-        new_height = round(abs(self.inp.shape[0]*COS)+abs(self.inp.shape[1]*SIN))+1
-        new_width = round(abs(self.inp.shape[1]*COS)+abs(self.inp.shape[0]*SIN))+1
+        new_height = round(abs(self.__inp.shape[0]*COS)+abs(self.__inp.shape[1]*SIN))+1
+        new_width = round(abs(self.__inp.shape[1]*COS)+abs(self.__inp.shape[0]*SIN))+1
 
         # creating template in output with new dimensions
-        self.outp = np.zeros((new_height,new_width,self.inp.shape[2]))
+        self.__outp = np.zeros((new_height,new_width,self.__inp.shape[2]))
 
         # finding centre about which image must be rotated
         # calculated wrt original image
-        og_centre_height = round(((self.inp.shape[0]+1)/2)-1)
-        og_centre_width = round(((self.inp.shape[1]+1)/2)-1)
+        og_centre_height = round(((self.__inp.shape[0]+1)/2)-1)
+        og_centre_width = round(((self.__inp.shape[1]+1)/2)-1)
 
         
         # finding centre of the new image that will be obtained
@@ -146,8 +146,8 @@ class imageEdit:
         for i in range(height):
             for j in range(width):
                 # coords of pixel wrt the centre of original shape
-                y = self.inp.shape[0]-1-i-og_centre_height
-                x = self.inp.shape[1]-1-j-og_centre_width
+                y = self.__inp.shape[0]-1-i-og_centre_height
+                x = self.__inp.shape[1]-1-j-og_centre_width
 
                 # coords of pixel wrt to the rotated matrix
                 new_y = round(-x*SIN+y*COS)
@@ -160,42 +160,42 @@ class imageEdit:
 
                 # adding if check to prevent any errors while procesing
                 if 0<=new_x < new_width and 0<=new_y<new_height and new_x>=0 and new_y>=0:
-                    self.outp[new_y,new_x,:] = self.inp[i,j,:]      # copying pixel value to given index             
+                    self.__outp[new_y,new_x,:] = self.__inp[i,j,:]      # copying pixel value to given index             
 
         # not sure why this line is required but without this I was getting value errors
         # stating that the RGB values are floating point/ int32 etc and that they 
         # are supposed to be of type uint8
-        self.outp = self.outp.astype(np.uint8)
+        self.__outp = self.__outp.astype(np.uint8)
 
-        return self.outp
+        return self.__outp
 
 
     def invertColor(self):
-        self.outp = self.inp.copy()
+        self.__outp = self.__inp.copy()
 
-        self.outp = ~self.outp
+        self.__outp = ~self.__outp
 
-        # for i in range(len(self.inp)):
-        #     for j in range(len(self.inp[i])):
-        #         self.outp[i][j][0] = 255 - self.outp[i][j][0]
-        #         self.outp[i][j][1] = 255 - self.outp[i][j][1]
-        #         self.outp[i][j][2] = 255 - self.outp[i][j][2]
+        # for i in range(len(self.__inp)):
+        #     for j in range(len(self.__inp[i])):
+        #         self.__outp[i][j][0] = 255 - self.__outp[i][j][0]
+        #         self.__outp[i][j][1] = 255 - self.__outp[i][j][1]
+        #         self.__outp[i][j][2] = 255 - self.__outp[i][j][2]
 
 
     def contrast(self):
-        # pixvals = ((self.inp - self.inp.min)/(self.inp.max() - self.inp.min()))*255
+        # pixvals = ((self.__inp - self.__inp.min)/(self.__inp.max() - self.__inp.min()))*255
         percentage = int(input("Enter contrast percentage: "))
         self.multiplier = int(percentage/100 * 255)
 
-        minval = np.percentile(self.inp, 2)
-        maxval = np.percentile(self.inp, 98)
+        minval = np.percentile(self.__inp, 2)
+        maxval = np.percentile(self.__inp, 98)
 
-        pixvals = np.clip(self.inp, minval, maxval)
+        pixvals = np.clip(self.__inp, minval, maxval)
         pixvals = ((pixvals - minval) / (maxval - minval))*self.multiplier
 
-        self.outp = pixvals.astype(np.uint8)
+        self.__outp = pixvals.astype(np.uint8)
 
-        return self.outp
+        return self.__outp
 
 
     def rgbchannel(self):
@@ -206,34 +206,34 @@ class imageEdit:
 
         if re.search(reg,self.channel):
             
-            self.outp = np.zeros(self.inp.shape)
+            self.__outp = np.zeros(self.__inp.shape)
 
             if 'r' in self.channel:
-                self.outp[:,:,0] = self.inp[:,:,0]
+                self.__outp[:,:,0] = self.__inp[:,:,0]
             if 'g' in self.channel:
-                self.outp[:,:,1] = self.inp[:,:,1]
+                self.__outp[:,:,1] = self.__inp[:,:,1]
             if 'b' in self.channel:
-                self.outp[:,:,2] = self.inp[:,:,2]
+                self.__outp[:,:,2] = self.__inp[:,:,2]
 
-            self.outp = self.outp.astype(np.uint8)
+            self.__outp = self.__outp.astype(np.uint8)
 
 
         else:
             print("Invalid input composition")
 
-        return self.outp
+        return self.__outp
 
 
     def transparency(self):
-        self.outp = np.zeros((self.inp.shape[0],self.inp.shape[1],4)).astype(np.uint8)
+        self.__outp = np.zeros((self.__inp.shape[0],self.__inp.shape[1],4)).astype(np.uint8)
 
         self.percentage = float(input("Enter transparency percentage: "))
         self.multiplier = int((100-self.percentage)/100 * 255)
 
-        self.outp[:,:,0:3] = self.inp[:,:,0:3]
-        self.outp[:,:,3] = self.multiplier
+        self.__outp[:,:,0:3] = self.__inp[:,:,0:3]
+        self.__outp[:,:,3] = self.multiplier
 
-        return self.outp
+        return self.__outp
 
 
         
